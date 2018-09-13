d4362ed09512e521d644f5fef9c30f9c09ffef3c
IanLoam126 <league@iMac-7.attlocal.net>
2018-09-12 19:50:51 -0700

    Added SideBar and deleted MenuBar

--- mouse/src/App.js
+++ mouse/src/App.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import leaguelogo from './leaguelogo.png';
 import ucsd from './ucsd.png';
 import './App.css';
-
-import MenuAppBar from './MenuAppBar';
+import SideBar from './SideBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MenuAppBar />
+        <SideBar />
         <header className="App-header">
         <div>
-          <img src={leaguelogo} alt="leaguelogo" height="160px" /> 
-          <img src={ucsd} alt="ucsd" height="160px" />
+          <img class="logo-league" src={leaguelogo} alt="leaguelogo" height="160px" /> 
+          <img class="logo-UCSD" src={ucsd} alt="ucsd" height="160px" />
         </div>
         </header>
         <p className="App-intro">

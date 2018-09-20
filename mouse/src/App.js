58b772b9158a602b73decb0afed5d7eb956a326f
IanLoam126 <league@iMac-7.attlocal.net>
2018-09-19 19:26:22 -0700

    Added Panel class in React

--- mouse/src/App.js
+++ mouse/src/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import leaguelogo from './leaguelogo.png';
 import ucsd from './ucsd.png';
 import './App.css';
+import Card from '@material-ui/core/Card';
 import SideBar from './SideBar';
+import Panel from './Panel'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  };
   render() {
     return (
       <div className="App">
@@ -15,6 +20,7 @@ class App extends Component {
           <img class="logo-UCSD" src={ucsd} alt="ucsd" height="160px" />
         </div>
         </header>
+        <Panel/>
         <p className="App-intro">
          
         </p>

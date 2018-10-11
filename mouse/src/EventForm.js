7d822750d96e81f5814993fbbf0615155922ed72
League <league@iMac-7.attlocal.net>
2018-10-10 19:36:51 -0700

    Attempted at using Axios

--- mouse/src/EventForm.js
+++ mouse/src/EventForm.js
@@ -3,41 +3,62 @@ import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import axios from 'axios';
+const instance = axios.create({baseURL: 'http://localhost:8080'})
+
 
 class EventForm extends React.Component {
 
 constructor() {
      super();
-     this.state = {};
+     this.state = {
+       'eventName':'',
+       'date':'',
+       'repeats':false
+     };
+  }
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   }
   handleChange = name => event => {
     this.setState({ [name]: event.target.checked });
   };
-  handleSubmit = (event) => {
-     //Make a network call somewhere
+  onSubmit = (e) => {
+    e.preventDefault();
+    // get our form data out of state
+    const { eventName, date, repeats } = this.state;
 
-     event.preventDefault();
+    axios.post('${proccess.env.HOST}:8080/mice', { eventName, date, repeats })
+      .then((result) => {
+        console.log("it works!");
+      });
   }
 
   render() {
+    const {eventName, date, repeats} = this.state;
      return( 
-        <form method='post' action='http://localhost:8080/event'>
-          <TextField label="Event Name"  onChange={this.change} />
+        <form onSubmit={this.onSubmit}>
+          <TextField label="Event Name" name="eventName"  value={this.state.eventName} onChange={this.onChange} />
 
           <TextField
             id="date"
+            name="date"
             label="Date"
             type="date"
+            value={this.state.date}
             defaultValue="2019-01-01"
             InputLabelProps={{
               shrink: true,
             }}
+            onChange={this.onChange}
     />
           <FormControlLabel
           control={
           <Checkbox
-            checked={this.state.checkedB}
+          value={this.state.repeats}
+            name="repeats"
             onChange={this.handleChange('checkedB')}
+            onChange={this.onChange}
             value="checkedB"
             color="primary"
             
